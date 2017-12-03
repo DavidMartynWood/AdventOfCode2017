88b00653eff4c7a40d97fe59cc35c97f1db6f777
David Wood <david.wood@saabgroup.com>
2017-12-03 21:38:43 +0000

    Add project files.

---
+++ Day1/InverseCaptcha.fs
@@ -0,0 +1,17 @@
+﻿namespace Day1
+
+type InverseCaptcha() =
+    let evaluateItem (x, y) = 
+        if (x = y) then
+            x
+        else
+            0;
+
+    let rec sumListTailRecHelper (accumulator, comparer, xs) =
+        match xs with 
+        | []    -> accumulator 
+        | y::ys -> sumListTailRecHelper (accumulator + evaluateItem(y, comparer), y, ys);
+
+    member this.Evaluate(input: int array) =
+        evaluateItem(input.[0], input.[input.Length - 1]) +
+        sumListTailRecHelper(0, input.[0], List.ofArray(input.[1 .. input.Length - 1]));
