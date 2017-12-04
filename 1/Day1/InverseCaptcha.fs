b1e0958e7f28f91848e793d9c9d0118da0d7dbc5
David Wood <david.wood@saabgroup.com>
2017-12-04 21:59:45 +0000

    Day one part 2

--- 1/Day1/InverseCaptcha.fs
+++ 1/Day1/InverseCaptcha.fs
@@ -11,7 +11,17 @@ type InverseCaptcha() =
         match xs with 
         | []    -> accumulator 
         | y::ys -> sumListTailRecHelper (accumulator + evaluateItem(y, comparer), y, ys);
+    
+    let rec sumListTailRecHelper2 (accumulator, xs, ys) =
+        match xs, ys with 
+        | [], _   -> accumulator 
+        | _, []   -> accumulator 
+        | xHead::xTail, yHead::yTail -> sumListTailRecHelper2 (accumulator + evaluateItem(xHead, yHead), xTail, yTail);
 
     member this.Evaluate(input: int array) =
         evaluateItem(input.[0], input.[input.Length - 1]) +
         sumListTailRecHelper(0, input.[0], List.ofArray(input.[1 .. input.Length - 1]));
+
+    member this.EvaluatePart2(input: int array) =
+        let left, right = input |> Array.splitAt (input.Length / 2)
+        sumListTailRecHelper2(0, List.ofArray(input), List.ofArray(Array.concat([|right; left|])));
